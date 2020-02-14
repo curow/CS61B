4d9027e774e37054328d122a01fff4538b9fb937
Zhiwen Xie <TiwazBiu@gmail.com>
2020-02-14 23:55:37 +0800

    [project 1A]implemented ArrayDeque with all the required features

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -19,28 +19,53 @@ public class ArrayDeque<T> {
         return size == 0;
     }
 
-    private int addModuloLength(int a, int b) {
-        return (a + b) % items.length;
+    private int addThenModulo(int a, int b, int modulo) {
+        return (a + b) % modulo;
+    }
+
+    private int addThenModuloByLength(int a, int b) {
+        return addThenModulo(a, b, items.length);
+    }
+
+    private void resize(int capacity) {
+        T[] a = (T[]) new Object[capacity];
+        int front = addThenModuloByLength(nextFirst, 1);
+        int newNextFirst = 3;
+        int newNextLast = 4;
+        for (int i = 0; i < size; i++) {
+            a[newNextLast] = items[front];
+            newNextLast = addThenModulo(newNextLast, 1, capacity);
+            front = addThenModuloByLength(front, 1);
+        }
+        items = a;
+        nextFirst = newNextFirst;
+        nextLast = newNextLast;
     }
 
     public void addFirst(T item) {
+        if (size == items.length) {
+            resize(items.length * 2);
+        }
         size++;
         items[nextFirst] = item;
-        nextFirst = addModuloLength(nextFirst, -1);
+        nextFirst = addThenModuloByLength(nextFirst, -1);
     }
 
     public void addLast(T item) {
+        if (size == items.length) {
+            resize(items.length * 2);
+        }
         size++;
         items[nextLast] = item;
-        nextLast = addModuloLength(nextLast, 1);
+        nextLast = addThenModuloByLength(nextLast, 1);
     }
 
     public void printDeque() {
-        int first = addModuloLength(nextFirst, 1);
+        int first = addThenModuloByLength(nextFirst, 1);
         for (int i = 0; i < size; i++) {
-            int index = addModuloLength(first, i);
+            int index = addThenModuloByLength(first, i);
             System.out.print(items[index]);
-            if (index == addModuloLength(nextLast, -1)) {
+            if (index == addThenModuloByLength(nextLast, -1)) {
                 System.out.println();
             } else  {
                 System.out.print(" ");
@@ -48,14 +73,20 @@ public class ArrayDeque<T> {
         }
     }
 
+    private double usageRatio() {
+        return (double) size / items.length;
+    }
     public T removeFirst() {
         if (size == 0) {
             return null;
         } else {
             size--;
-            nextFirst = addModuloLength(nextFirst, 1);
+            nextFirst = addThenModuloByLength(nextFirst, 1);
             T item = items[nextFirst];
             items[nextFirst] = null;
+            if (usageRatio() < 0.25) {
+                resize(items.length / 2);
+            }
             return item;
         }
     }
@@ -65,15 +96,18 @@ public class ArrayDeque<T> {
             return null;
         } else {
             size--;
-            nextLast = addModuloLength(nextLast, -1);
+            nextLast = addThenModuloByLength(nextLast, -1);
             T item = items[nextLast];
             items[nextLast] = null;
+            if (usageRatio() < 0.25) {
+                resize(items.length / 2);
+            }
             return item;
         }
     }
 
     public T get(int index) {
-        int front = addModuloLength(nextFirst, 1);
+        int front = addThenModuloByLength(nextFirst, 1);
         return items[front + index];
     }
 
@@ -92,10 +126,21 @@ public class ArrayDeque<T> {
         System.out.println("head: " + head);
         int tail = L.removeLast();
         System.out.println("tail: " + tail);
-        System.out.println(L.size());
+        System.out.println("size: " + L.size());
         L.printDeque();
-        for (int i = 0; i < L.size(); i++) {
-            System.out.print(L.get(i) + " ");
+        L.resize(20);
+        L.printDeque();
+
+        for (int i = 0; i < 100; i++) {
+            L.addLast(i);
         }
+        L.printDeque();
+        System.out.println("size: " + L.size());
+
+        for (int i = 0; i < 100; i++) {
+            L.removeLast();
+        }
+        L.printDeque();
+        System.out.println("size: " + L.size());
     }
 }
