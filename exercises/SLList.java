c645b811efe478597a37b2d48971769f508cef53
Zhiwen Xie <TiwazBiu@gmail.com>
2020-02-14 15:06:32 +0800

    [discussion 03]add reverse(both recursive and iterative) to SLList

--- exercises/SLList.java
+++ exercises/SLList.java
@@ -69,6 +69,46 @@ public class SLList {
         System.out.println();
     }
 
+    public void reverse() {
+        if (sentinel.next == null || sentinel.next.next == null) {
+            return;
+        }
+        IntNode tail = sentinel.next;
+        IntNode currentNode = sentinel.next.next;
+        tail.next = null;
+        while (currentNode != null) {
+            IntNode head = sentinel.next;
+            IntNode nextNode = currentNode.next;
+
+            sentinel.next = currentNode;
+            currentNode.next = head;
+
+            currentNode = nextNode;
+        }
+    }
+
+    private IntNode reverse(IntNode L1, IntNode L2) {
+        if (L2 == null) {
+            return L1;
+        } else {
+            IntNode p = L2;
+            L2 = L2.next;
+            p.next = L1;
+            return reverse(p, L2);
+        }
+    }
+
+    public void reverseRecursive() {
+        if (sentinel.next == null) {
+            return;
+        }
+        IntNode L1 = sentinel.next;
+        IntNode L2 = sentinel.next.next;
+        L1.next = null;
+        sentinel.next = reverse(L1, L2);
+
+    }
+
     public static void main(String[] args) {
         SLList L = new SLList();
         L.addFirst(3);
@@ -82,6 +122,12 @@ public class SLList {
         L.insert(78, 1);
         L.insert(23, 5);
         L.insert(13, 234);
+        L.printList();
+
+        L.reverse();
+        L.printList();
+
+        L.reverseRecursive();
         L.printList();
     }
 }
