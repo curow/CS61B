4e38ed0ac205504023cbe0ccece3044504d0e8ab
Zhiwen Xie <TiwazBiu@gmail.com>
2020-02-19 17:59:22 +0800

    [exercise 6.2-6.3]add throw expression and implemented iterator

--- 2019/lecture-code/exercises/DIY/inheritance4/ArraySet.java
+++ 2019/lecture-code/exercises/DIY/inheritance4/ArraySet.java
@@ -1,4 +1,7 @@
-public class ArraySet<T> {
+import java.util.Iterator;
+import java.util.Set;
+
+public class ArraySet<T> implements Iterable<T> {
     private int size;
     private T[] items;
 
@@ -8,6 +11,28 @@ public class ArraySet<T> {
         items = (T[]) new Object[100];
     }
 
+    @Override
+    public Iterator<T> iterator() {
+        return new SetIterator();
+    }
+
+    class SetIterator implements Iterator<T> {
+        private int p;
+
+        public SetIterator() {
+            p = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return p < size;
+        }
+
+        @Override
+        public T next() {
+            return items[p++];
+        }
+    }
     public boolean contains(T x) {
         for (int i = 0; i < size; i++) {
             if (items[i].equals(x)) {
@@ -18,7 +43,12 @@ public class ArraySet<T> {
     }
 
     public void add(T x) {
-        items[size++] = x;
+        if (x == null) {
+            throw new IllegalArgumentException("you can't add null to " +
+                    "an ArraySet");
+        } else if (!contains(x)) {
+            items[size++] = x;
+        }
     }
 
     public int size() {
@@ -27,13 +57,17 @@ public class ArraySet<T> {
 
     public static void main(String[] args) {
         ArraySet<String> s = new ArraySet<>();
-        s.add(null);
+//        s.add(null);
         s.add("horse");
         s.add("fish");
         s.add("house");
         s.add("fish");        
         System.out.println(s.contains("horse"));        
-        System.out.println(s.size());       
+        System.out.println(s.size());
+
+        for (String str : s) {
+            System.out.println(str);
+        }
     }
 
     /* Also to do:
