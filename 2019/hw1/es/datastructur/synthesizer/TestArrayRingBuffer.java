24ea7c9bb02e8a32adcaa3dd2606783f61e18207
Zhiwen Xie <TiwazBiu@gmail.com>
2020-02-21 11:55:19 +0800

    [2019 homework1]completed task2: implemented ArrayRingBuffer

--- 2019/hw1/es/datastructur/synthesizer/TestArrayRingBuffer.java
+++ 2019/hw1/es/datastructur/synthesizer/TestArrayRingBuffer.java
@@ -9,6 +9,31 @@ import static org.junit.Assert.*;
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer<Integer> arb = new ArrayRingBuffer<>(10);
+        assertEquals(10, arb.capacity());
+        assertEquals(0, arb.fillCount());
+        assertTrue(arb.isEmpty());
+        arb.enqueue(30);
+        assertEquals(30, (int) arb.peek());
+        assertEquals(1, arb.fillCount());
+        int output = arb.dequeue();
+        assertEquals(30, output);
+        for (int i = 0; i < 10; i++) {
+            arb.enqueue(i);
+        }
+        assertEquals(10, arb.fillCount());
+        assertTrue(arb.isFull());
+    }
+
+    @Test
+    public void cornerCaseTest() {
+        ArrayRingBuffer<Integer> arb = new ArrayRingBuffer<>(1);
+        assertTrue(arb.isEmpty());
+        arb.enqueue(23);
+        assertTrue(arb.isFull());
+        assertEquals(23, (int) arb.peek());
+        int output = arb.dequeue();
+        assertTrue(arb.isEmpty());
+        assertEquals(23, output);
     }
 }
