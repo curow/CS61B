3fe57b89d81fbb991eac3e3a3cf62463eb55283f
Zhiwen Xie <TiwazBiu@gmail.com>
2020-03-03 14:43:22 +0800

    [2019 homework3: HashCode]finished SimpleOomage

--- 2019/hw3/hw3/hash/TestSimpleOomage.java
+++ 2019/hw3/hw3/hash/TestSimpleOomage.java
@@ -22,10 +22,17 @@ public class TestSimpleOomage {
 
     @Test
     public void testHashCodePerfect() {
-        /* TODO: Write a test that ensures the hashCode is perfect,
-          meaning no two SimpleOomages should EVER have the same
-          hashCode UNLESS they have the same red, blue, and green values!
-         */
+        Set<Integer> allValidHashCode = new HashSet<Integer>();
+        for (int i = 0; i <= 255; i += 5) {
+            for (int j = 0; j <= 255; j += 5) {
+                for (int k = 0; k <= 255; k += 5) {
+                    SimpleOomage guy = new SimpleOomage(i, j, k);
+                    Integer guyCode = guy.hashCode();
+                    assertFalse(allValidHashCode.contains(guyCode));
+                    allValidHashCode.add(guyCode);
+                }
+            }
+        }
     }
 
     @Test
@@ -39,7 +46,6 @@ public class TestSimpleOomage {
         assertNotEquals(ooA, "ketchup");
     }
 
-    /*
     @Test
     public void testHashCodeAndEqualsConsistency() {
         SimpleOomage ooA = new SimpleOomage(5, 10, 20);
@@ -47,10 +53,9 @@ public class TestSimpleOomage {
         HashSet<SimpleOomage> hashSet = new HashSet<>();
         hashSet.add(ooA);
         assertTrue(hashSet.contains(ooA2));
-    }*/
+    }
 
-    /* TODO: Uncomment this test after you finish haveNiceHashCodeSpread in OomageTestUtility */
-    /*@Test
+    @Test
     public void testRandomOomagesHashCodeSpread() {
         List<Oomage> oomages = new ArrayList<>();
         int N = 10000;
@@ -60,7 +65,7 @@ public class TestSimpleOomage {
         }
 
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(oomages, 10));
-    }*/
+    }
 
     /** Calls tests for SimpleOomage. */
     public static void main(String[] args) {
