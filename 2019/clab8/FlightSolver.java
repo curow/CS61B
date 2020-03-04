3bcfd341a3832088d3eebf8c20af345bb1cd0425
Zhiwen Xie <TiwazBiu@gmail.com>
2020-03-04 20:19:07 +0800

    [Challenge Lab 8: Heaps and Hashes] implemented FlightSolver based on 2018 CS61B midterm2 video idea

--- 2019/clab8/FlightSolver.java
+++ 2019/clab8/FlightSolver.java
@@ -12,49 +12,35 @@ import java.util.PriorityQueue;
 
 public class FlightSolver {
     private PriorityQueue<Flight> minStartTimeQueue;
+    private PriorityQueue<Flight> minEndTimeQueue;
 
     public FlightSolver(ArrayList<Flight> flights) {
         Comparator<Flight> minStartTimeComparator =
                 Comparator.comparingInt(Flight::startTime);
         minStartTimeQueue = new PriorityQueue<>(minStartTimeComparator);
         minStartTimeQueue.addAll(flights);
+        Comparator<Flight> minEndTimeComparator =
+                Comparator.comparingInt(Flight::endTime);
+        minEndTimeQueue = new PriorityQueue<>(minEndTimeComparator);
+        minEndTimeQueue.addAll(flights);
     }
 
     public int solve() {
         int maxPassengers = -1;
+        int totalPassengers = 0;
         while (!minStartTimeQueue.isEmpty()) {
-            Flight flight0 = minStartTimeQueue.poll();
-            if (flight0.passengers > maxPassengers) {
-                maxPassengers = flight0.passengers;
-            }
-            if (!minStartTimeQueue.isEmpty()) {
-                Flight flight1 = minStartTimeQueue.peek();
-                if (flight0.endTime >= flight1.startTime) { // overlapping
-                    minStartTimeQueue.poll();
-                    int endTime = Math.min(flight0.endTime, flight1.endTime);
-                    Flight overlappingFlight = new Flight(
-                      flight1.startTime,
-                      endTime,
-                      flight0.passengers + flight1.passengers
-                    );
-                    minStartTimeQueue.add(overlappingFlight);
-                    int startTime = endTime + 1;
-                    if (startTime <= flight0.endTime) {
-                        Flight remainingFlight0 = new Flight(
-                                startTime,
-                                flight0.endTime,
-                                flight0.passengers
-                        );
-                        minStartTimeQueue.add(remainingFlight0);
-                    } else if (startTime <= flight1.endTime) {
-                        Flight remainingFlight1 = new Flight(
-                                startTime,
-                                flight1.endTime,
-                                flight1.passengers
-                        );
-                        minStartTimeQueue.add(remainingFlight1);
-                    }
+            Flight startFlight = minStartTimeQueue.peek();
+            Flight endFlight = minEndTimeQueue.peek();
+            if (endFlight == null
+                    || startFlight.startTime <= endFlight.endTime) {
+                totalPassengers += startFlight.passengers;
+                if (totalPassengers > maxPassengers) {
+                    maxPassengers = totalPassengers;
                 }
+                minStartTimeQueue.poll();
+            } else {
+                totalPassengers -= endFlight.passengers;
+                minEndTimeQueue.poll();
             }
         }
         return maxPassengers;
