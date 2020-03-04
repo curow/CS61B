d3e8cb1573bc6ab71a48f27c9be2e6cac4c74c0f
Zhiwen Xie <TiwazBiu@gmail.com>
2020-03-04 22:51:52 +0800

    [Challenge Lab 8: Heaps and Hashes] implemented Rabin–Karp algorithm

--- 2019/clab8/RabinKarpAlgorithm.java
+++ 2019/clab8/RabinKarpAlgorithm.java
@@ -3,9 +3,32 @@ public class RabinKarpAlgorithm {
 
     /**
      * This algorithm returns the starting index of the matching substring.
-     * This method will return -1 if no matching substring is found, or if the input is invalid.
+     * This method will return -1 if no matching substring is found,
+     * or if the input is invalid.
      */
     public static int rabinKarp(String input, String pattern) {
+        if (pattern.length() > input.length()) {
+            return -1;
+        }
+        int size = pattern.length();
+        RollingString rs = new RollingString(input.substring(0, size), size);
+        RollingString patternRS = new RollingString(pattern, size);
+        int patternHash = patternRS.hashCode();
+        if (rs.hashCode() == patternHash) {
+            return 0;
+        }
+
+        int end = size;
+        while (end < input.length()) {
+            rs.addChar(input.charAt(end));
+            if (rs.hashCode() == patternHash) {
+                break;
+            }
+            end++;
+        }
+        if (end < input.length()) {
+            return end - size + 1;
+        }
         return -1;
     }
 
--- 2019/clab8/RollingString.java
+++ 2019/clab8/RollingString.java
@@ -1,3 +1,5 @@
+import java.lang.management.ManagementFactory;
+
 /**
  * A String-like class that allows users to add and remove characters in the String
  * in constant time and have a constant-time hash function. Used for the Rabin-Karp
@@ -17,13 +19,52 @@ class RollingString{
      */
     static final int PRIMEBASE = 6113;
 
+    private static class Node {
+        public int value;
+        public Node next;
+        public Node prev;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    private Node sentinel;
+    private int hashCode;
+    private int highestWeight;
+
     /**
      * Initializes a RollingString with a current value of String s.
      * s must be the same length as the maximum length.
      */
     public RollingString(String s, int length) {
         assert(s.length() == length);
-        /* FIX ME */
+        sentinel = new Node(length);
+        Node last = null;
+        Node current = sentinel;
+        for (char ch : s.toCharArray()) {
+            last = new Node((int) ch);
+            current.next = last;
+            last.prev = current;
+            current = last;
+        }
+        assert last != null;
+        last.next = sentinel;
+        sentinel.prev = last;
+
+        current = sentinel.next;
+        for (hashCode = 0; current != sentinel; current = current.next) {
+            hashCode *= Math.floorMod(UNIQUECHARS, PRIMEBASE);
+            hashCode = Math.floorMod(hashCode, PRIMEBASE);
+            hashCode += Math.floorMod(current.value, PRIMEBASE);
+            hashCode = Math.floorMod(hashCode, PRIMEBASE);
+        }
+
+        highestWeight = 1;
+        for (int i = 0; i < length - 1; i++) {
+            highestWeight *= Math.floorMod(UNIQUECHARS, PRIMEBASE);
+            highestWeight = Math.floorMod(highestWeight, PRIMEBASE);
+        }
     }
 
     /**
@@ -32,7 +73,22 @@ class RollingString{
      * Should be a constant-time operation.
      */
     public void addChar(char c) {
-        /* FIX ME */
+        Node first = sentinel.next;
+        Node last = sentinel.prev;
+        Node current = new Node((int) c);
+        sentinel.next = first.next;
+        sentinel.next.prev = sentinel;
+        last.next = current;
+        current.prev = last;
+        current.next = sentinel;
+        sentinel.prev = current;
+
+        hashCode -= Math.floorMod(first.value * highestWeight, PRIMEBASE);
+        hashCode = Math.floorMod(hashCode, PRIMEBASE);
+        hashCode *= Math.floorMod(UNIQUECHARS, PRIMEBASE);
+        hashCode = Math.floorMod(hashCode, PRIMEBASE);
+        hashCode += c;
+        hashCode = Math.floorMod(hashCode, PRIMEBASE);
     }
 
 
@@ -43,8 +99,11 @@ class RollingString{
      */
     public String toString() {
         StringBuilder strb = new StringBuilder();
-        /* FIX ME */
-        return "";
+        for (Node current = sentinel.next; current != sentinel; current =
+                current.next) {
+            strb.append((char) current.value);
+        }
+        return strb.toString();
     }
 
     /**
@@ -52,8 +111,7 @@ class RollingString{
      * Should be a constant-time operation.
      */
     public int length() {
-        /* FIX ME */
-        return -1;
+        return sentinel.value;
     }
 
 
@@ -64,8 +122,9 @@ class RollingString{
      */
     @Override
     public boolean equals(Object o) {
-        /* FIX ME */
-        return false;
+        if (o == null) return false;
+        if (this.getClass() != o.getClass()) return false;
+        return toString().equals(o.toString());
     }
 
     /**
@@ -74,7 +133,6 @@ class RollingString{
      */
     @Override
     public int hashCode() {
-        /* FIX ME */
-        return -1;
+        return hashCode;
     }
 }
