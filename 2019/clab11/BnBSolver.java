ebbef38fd1ee2eccc2657a1f20301365e5969183
Zhiwen Xie <TiwazBiu@gmail.com>
2020-03-22 17:05:49 +0800

    [clab11: Bears and Beds]implemented BnBSolver with time complexity N^2

--- 2019/clab11/BnBSolver.java
+++ 2019/clab11/BnBSolver.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -9,24 +10,38 @@ import java.util.List;
  * size as the ith Bed.
  */
 public class BnBSolver {
+    private List<Bear> bears;
+    private List<Bed> beds;
 
     public BnBSolver(List<Bear> bears, List<Bed> beds) {
-        // TODO: Fix me.
+       this.bears = bears;
+       this.beds = new ArrayList<>();
+       List<Boolean> occupied = new ArrayList<>(beds.size());
+       for (int i = 0; i < beds.size(); i++) {
+           occupied.add(i, false);
+       }
+       for (Bear bear : bears) {
+           int index = 0;
+           for (Bed bed : beds) {
+               if (!occupied.get(index) && bear.compareTo(bed) == 0) {
+                   this.beds.add(bed);
+               }
+               ++index;
+           }
+       }
     }
 
     /**
      * Returns List of Bears such that the ith Bear is the same size as the ith Bed of solvedBeds().
      */
     public List<Bear> solvedBears() {
-        // TODO: Fix me.
-        return null;
+        return bears;
     }
 
     /**
      * Returns List of Beds such that the ith Bear is the same size as the ith Bear of solvedBears().
      */
     public List<Bed> solvedBeds() {
-        // TODO: Fix me.
-        return null;
+        return beds;
     }
 }
